1634c8f8f9b9b383e61666eabe3ed36b646b5698
oleg_ho <9656173+oleg-ho@users.noreply.github.com>
2018-11-11 15:00:09 +0200

    first screen messages

---
+++ Assets/Hacker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Terminal.WriteLine("Hello %username%!");
+        Terminal.WriteLine("Want to hack, huh?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 to hack store");
+        Terminal.WriteLine("Press 2 to hack ATM");
+        Terminal.WriteLine("Press 3 to hack GYM");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Make your chose:");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
